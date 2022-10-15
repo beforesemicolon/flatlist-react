cce8f7c0f337e073beee99231d75a4b6e482f504
Elson Correia <design@elsoncorreia.com>
2022-10-15 03:47:16 -0400

    expose interfaces

--- src/index.tsx
+++ src/index.tsx
@@ -6,5 +6,9 @@ export {default as filterList} from './___utils/filterList';
 export {default as groupList} from './___utils/groupList';
 export {default as limitList} from './___utils/limitList';
 export {default as PlainList} from './___subComponents/PlainList';
+export {GroupInterface, SortInterface, ScrollToTopInterface} from './props';
+export {DisplayInterface} from './___subComponents/DisplayHandler';
+export {SearchOptionsInterface} from './___utils/searchList';
+export {InfiniteLoaderProps as InfiniteLoaderInterface} from './___subComponents/InfiniteLoader';
 
 export default FlatList;
